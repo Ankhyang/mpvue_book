1eccc999f0937aa5f2f716c750444f4d22393cee
Ankhyang <ankh3690@163.com>
2019-12-17 17:16:48 +0800

    首页轮播

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
-
-Vue.config.productionTip = false
+// 设置为 false 以阻止 vue 在启动时生成生产提示。
+Vue.prototype.productionTip = false
+// 声明当前组件为整个应用
 App.mpType = 'app'
-
+// 生成当前应用的实例
 const app = new Vue(App)
+// 挂载当前应用的实例
 app.$mount()
---
+++ src/pages/firstView/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="swiper_div">
+    <swiper indicator-dots indicator-color="#cccccc" indicator-active-color="#4D79FF" circular>
+      <swiper-item>
+        <img src="/static/images/firstView/1.jpg" alt="">
+      </swiper-item>
+      <swiper-item>
+        <img src="/static/images/firstView/2.jpg" alt="">
+      </swiper-item>
+      <swiper-item>
+        <img src="/static/images/firstView/3.jpg" alt="">
+      </swiper-item>
+      <swiper-item>
+        <img src="/static/images/firstView/nvsheng.jpg" alt="">
+      </swiper-item>
+    </swiper> 
+    <button plain>开始体验</button>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #swiper_div
+    width: 100%
+    height: 100%
+    swiper
+      width: 100%
+      height: 100%
+      swiper-item
+        width: 100%
+        height: 100%
+        img 
+          width: 100%
+          height: 100%  
+    button
+      width 280rpx
+      height  80rpx
+      text-align center
+      line-height 80rpx
+      position absolute
+      bottom 20rpx
+      left 50%
+      margin-left -140rpx
+      background rgba(255, 255, 255, .8)
+      border 2rpx solid #fcfcfc
+</style>
